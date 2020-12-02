91681576af9dd0bcf34c4eefb2267f198299327f
jnodre <josunoda@gmail.com>
2020-12-02 19:23:43 +0000

    refresco de local storage al hacer patch

--- src/app/services/patchuser.service.ts
+++ src/app/services/patchuser.service.ts
@@ -1,52 +1,95 @@
 import { Injectable } from '@angular/core';
 import axios from 'axios';
 import { User } from '@int/user';
+import { GetuserService } from '@app/services/getuser.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class PatchuserService {
   dburl = 'http://localhost:3000/users/';
-  constructor() {}
+  constructor(private getuserService: GetuserService) {}
 
   public editHobbies(id: string, data: string[]): Promise<User> {
     return axios.patch(this.dburl + id + '/hobbies', {
       hobbies: data,
     });
   }
-  public editName(id: string, data: string): Promise<User> {
-    return axios.patch(this.dburl + id + '/name', {
-      name: data,
-    });
+  public editName(id: string, data: string): Promise<any> {
+    return axios
+      .patch(this.dburl + id + '/name', {
+        name: data,
+      })
+      .then(() =>
+        this.getuserService
+          .getUser(id)
+          .then((res) => localStorage.setItem('user', JSON.stringify(res)))
+      );
   }
-  public editGender(id: string, data: string): Promise<User> {
-    return axios.patch(this.dburl + id + '/gender', {
-      gender: data,
-    });
+  public editGender(id: string, data: string): Promise<any> {
+    return axios
+      .patch(this.dburl + id + '/gender', {
+        gender: data,
+      })
+      .then(() =>
+        this.getuserService
+          .getUser(id)
+          .then((res) => localStorage.setItem('user', JSON.stringify(res)))
+      );
   }
-  public editEmail(id: string, data: string): Promise<User> {
-    return axios.patch(this.dburl + id + '/email', {
-      email: data,
-    });
+  public editEmail(id: string, data: string): Promise<any> {
+    return axios
+      .patch(this.dburl + id + '/email', {
+        email: data,
+      })
+      .then(() =>
+        this.getuserService
+          .getUser(id)
+          .then((res) => localStorage.setItem('user', JSON.stringify(res)))
+      );
   }
-  public editPassword(id: string, data: string): Promise<User> {
-    return axios.patch(this.dburl + id + '/password', {
-      password: data,
-    });
+  public editPassword(id: string, data: string): Promise<any> {
+    return axios
+      .patch(this.dburl + id + '/password', {
+        password: data,
+      })
+      .then(() =>
+        this.getuserService
+          .getUser(id)
+          .then((res) => localStorage.setItem('user', JSON.stringify(res)))
+      );
   }
-  public editWhatsapp(id: string, data: string): Promise<User> {
-    return axios.patch(this.dburl + id + '/whatsapp', {
-      Whatssap: data,
-    });
+  public editWhatsapp(id: string, data: string): Promise<any> {
+    return axios
+      .patch(this.dburl + id + '/whatsapp', {
+        Whatssap: data,
+      })
+      .then(() =>
+        this.getuserService
+          .getUser(id)
+          .then((res) => localStorage.setItem('user', JSON.stringify(res)))
+      );
   }
-  public editFacebook(id: string, data: string): Promise<User> {
-    return axios.patch(this.dburl + id + '/facebook', {
-      Facebook: data,
-    });
+  public editFacebook(id: string, data: string): Promise<any> {
+    return axios
+      .patch(this.dburl + id + '/facebook', {
+        Facebook: data,
+      })
+      .then(() =>
+        this.getuserService
+          .getUser(id)
+          .then((res) => localStorage.setItem('user', JSON.stringify(res)))
+      );
   }
-  public editInstagram(id: string, data: string): Promise<User> {
-    return axios.patch(this.dburl + id + '/instagram', {
-      Instagram: data,
-    });
+  public editInstagram(id: string, data: string): Promise<any> {
+    return axios
+      .patch(this.dburl + id + '/instagram', {
+        Instagram: data,
+      })
+      .then(() =>
+        this.getuserService
+          .getUser(id)
+          .then((res) => localStorage.setItem('user', JSON.stringify(res)))
+      );
   }
 }
